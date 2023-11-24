788062a06500025335de3a88807f6d895dfb6023
Curtis Thompson <curtiswaynethompson@gmail.com>
2023-11-24 10:39:37 +0000

    Run and load queries in Python

--- database_connector.py
+++ database_connector.py
@@ -1,9 +1,32 @@
 import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect('data/data.db')
-cursor = conn.cursor()
 
-def execute_statement(statement):
+CONN = sqlite3.connect('data/data.db')
+CURSOR = CONN.cursor()
+
+
+def execute_statement(statement, cursor=CURSOR):
+    """Run a SQL query on the existing connection."""
     cursor.execute(statement)
-    return cursor.fetchall()
+    res = cursor.fetchall()
+    column_names = [description[0] for description in cursor.description]
+    return pd.DataFrame(res, columns=column_names)
+
+
+def load_query_from_file(file_path):
+    """Loads a query from a local .sql file."""
+    if file_path[-4:] != '.sql':
+        return None
+    
+    with open(file_path, 'r') as f:
+        query = f.read()
+    
+    return query
+
+
+def run_query_from_file(file_path):
+    """Load a query from a local .sql file, run it, and return result."""
+    query = load_query_from_file(file_path)
+    result = execute_statement(query)
+    return result
